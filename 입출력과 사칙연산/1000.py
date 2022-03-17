506f743bca1ac1bcbf25c5fec92878d4302937d4
supkoon <supkoon@gmail.com>
2022-03-17 12:01:31 +0900

    first commit

---
+++ 입출력과 사칙연산/1000.py
@@ -0,0 +1,7 @@
+a,b = list(map(int,input().split()))
+print(a+b)
+
+'''
+print(eval("+".join(input())))
+빠른방법 ! 
+'''
---
+++ 입출력과 사칙연산/1001.py
@@ -0,0 +1,8 @@
+a, b = map(int,input().split())
+print(a-b)
+
+#print(eval(''.join(input()[0,2])))
+
+
+
+
