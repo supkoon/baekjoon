b0d53b2138f3a02ff69f2dd01f773cf2663286ba
supkoon <supkoon@gmail.com>
2022-08-30 16:35:44 +0900

    08/30

--- 큐/1966.py
+++ 큐/1966.py
@@ -5,9 +5,14 @@ for case in range(n):
     n,m = map(int,input().split())
     papers = list(map(int,input().split()))
     queue=deque()
+    flag= True
     for i in papers:
         queue.append(i)
     while(queue):
         first = queue.popleft()
-        
-
+        for i in queue:
+            if first<i:
+                queue.append(first)
+                flag = False
+                break
+        if flag = pass
