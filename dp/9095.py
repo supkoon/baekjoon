8118a6584e9d13f960518f0dfdad96c42283a5b5
supkoon <supkoon@gmail.com>
2022-09-02 20:27:19 +0900

    09/02

--- dp/9095.py
+++ dp/9095.py
@@ -1,2 +1,21 @@
 n = int(input())
+dp = [0]*(10**6)
+
+1#1
+2#11,2
+3#111 ,12,21, 3
+4#1111 112 121 211 22 13 31
+5#11111 1112 1121 1211 2111 122 221 212 131 113 311 32 23
+# 1 2 4 7 13
+dp[1]=1
+dp[2]=2
+dp[3]=4
+for case in range(n):
+    num = int(input())
+    if dp[num]:
+        print(dp[num])
+    else:
+        for i in range(4,num+1):
+            dp[i] = dp[i-1]+dp[i-2]+dp[i-3]
+        print(dp[num])
 
