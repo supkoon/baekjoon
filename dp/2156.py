bf077c018e600b1d47d0328ff3bc3cab4f46c730
supkoon <supkoon@gmail.com>
2022-10-14 11:07:00 +0900

    10/14

--- dp/2156.py
+++ dp/2156.py
@@ -1,16 +1,14 @@
 import sys
 
-n = int(sys.stdin.readline().rstrip())
+n = int(sys.stdin.readline())
 nums =[]
 for i in range(n):
-    nums.append(int(sys.stdin.readline().rstrip()))
-
+    nums.append(int(sys.stdin.readline()))
 dp = [[0]*3 for _ in range(n)]
 dp[0]=[nums[0],nums[0],0]
 for i in range(1,n):
     for j in range(3):
-        dp[i][0]=dp[i-1][2]+nums[i]
-        dp[i][1]=dp[i-1][0]+nums[i]
-        dp[i][2]=dp[i-1][1]
-print(dp)
+        dp[i][0]=dp[i-1][2]+nums[i]#이전꺼 안먹음
+        dp[i][1]=dp[i-1][0]+nums[i]#이전꺼 먹음
+        dp[i][2]=max(dp[i-1])#이번에 안먹어야함
 print(max(dp[n-1]))
