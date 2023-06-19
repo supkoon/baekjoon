aa6bcd3470f863d0d6e3c426ab6414cbf1869d26
supkoon <supkoon@gmail.com>
2023-06-20 00:26:09 +0900

    0620

--- 구현/18111.py
+++ 구현/18111.py
@@ -9,14 +9,23 @@
 1. i,j의 블록을 제거하여 인벤토리에 넣기 - 2초
 2. 인벤토리에서 블럭을 꺼내 블록 쌓기 - 1초
 
-
-최저, 최대 찾기. 
-
+0부터 가장 높은 높이까지 모두 진행해보기 -brute force
+블록을 모두 회수해서, h*m*n <= num_block 인지 확인 
+확인 후, 최저 time이면 저장
 
 복잡도
 
+O(n^3)
+
 변수
 n,m,b : x,y,가진 블록수
+b_total : 총 블럭수
+curr_time : 현재 계산하는 경우의 시간
+gap : 각 블럭위치에서 블럭 수 차이
+
+highest : 최대 높이
+result_time : 만족하는 최저시간
+result_h : 만족하는 최대높이
 
 '''
 
@@ -26,34 +35,32 @@ graph = []
 for i in range(n):
     line = list(map(int,input().split()))
     graph.append(line)
+    for j in range(m):
+        if line[j]>highest:
+            highest=line[j]
 
 def fit():
-    global m,n,b
-    
-    smallest = 1e10
+    global n,m,b,graph
+    result_h = 0
+    result_time = 1e10
+    b_total = 0
     for i in range(n):
         for j in range(m):
-            result = 0
-            curr = b
-            h = graph[i][j] 
-            for k in range(n):
-                for l in range(m):
-                    gap = h-graph[k][l]
-                    if gap >= 0:
-                        result += gap
-                        curr-=gap
-                    elif gap < 0:
-                        result += gap*-2
-                        curr+=gap 
-                
-            if curr>=0:
-                if result == smallest  :
-                    if h > smallest_h:
-                        smallest_h = h
-                elif result < smallest :
-                    smallest_h = h
-                    smallest=result
-  
-    print(smallest,smallest_h)
-    
+            b_total+= graph[i][j]
+        
+    for h in range(highest+1):
+        if b_total>=h*m*n :
+            curr_time = 0 
+            for i in range(n):
+                for j in range(m):
+                    gap = h - graph[i][j]
+                    if gap>=0:
+                        curr_time += gap
+                    else:
+                        curr_time += gap*-2
+
+            if result_time >= curr_time:
+                result_h = h
+                result_time=curr_time
+    print(result_time,result_h)
 fit()
